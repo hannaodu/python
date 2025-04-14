17ccbda395f3e4cadcbed4229c72d05cbda9eb99
hanna <tennyalana@yahoo.com>
2025-04-14 13:46:37 +0100

    variable

--- comments.py
+++ comments.py
@@ -1,3 +1,9 @@
-print("currentky learning phyton which is part of programming fundamental")
+#this is a single-line comment
+print("currently learning phyton which is part of programming fundamental")
 
-print()
+# this is a multi-line comment
+"""
+infrastructure as a code
+integration with AWS
+automation and scripting
+"""
---
+++ variables.py
@@ -0,0 +1,9 @@
+#they are very important with everything we do .variables are containers for storing data values
+
+#variables must start with letter(a-z,A-Z or underscore)
+#can contains number and they are case-sensitive
+#can not be python keyworks (like,if,for,while etc)
+
+instance_name = "hanna-odulaja" #string
+instance_count = 4 #integar
+is_running = True #boolean variables
