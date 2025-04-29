e8f1767a2bb667cabb382a8ee3ea856f6543406d
hanna <tennyalana@yahoo.com>
2025-04-29 14:13:31 +0100

    Create indexing.py

---
+++ indexing.py
@@ -0,0 +1,10 @@
+#availability zone
+availability_zones = ["eu-west-1b","eu-west-2b","eu-west-3b"]
+print(f"numbers of AZ: {availability_zones}")
+
+#accessing list of items by index
+first_az = availability_zones[0]
+list_az = availability_zones[-1]
+print(f"First AZ: {first_az}")
+print(f"Last AZ: {list_az}")
+
